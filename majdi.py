ea0bf55e210042a081387113d1b0241e2942c997
Big-Tree <william.murphy333@gmail.com>
2018-10-31 13:35:51 +0000

    Added test set
    Test set has been added to the ROC curve, accuracy and loss plots.  Next
    step - save the best model and produce the ROC curve based on the based
    on the model that achieved the lowest validation loss.

--- majdi.py
+++ majdi.py
@@ -61,12 +61,21 @@ class LoadDataSet():
                     'class': index})
         random.shuffle(dataset_mixer)
         s_p = round(split_ratio*len(dataset_mixer))
+        e_p = len(dataset_mixer)
+        s_p_val_test = round((s_p+e_p)/2)
         self.train = {
             'data': np.asarray([_['image'] for _ in dataset_mixer][0 : s_p]),
             'labels': np.asarray([_['class'] for _ in dataset_mixer][0 : s_p])}
         self.val = {
-            'data': np.asarray([_['image'] for _ in dataset_mixer][s_p : ]),
-            'labels': np.asarray([_['class'] for _ in dataset_mixer][s_p : ])}
+            'data': np.asarray(
+                [_['image'] for _ in dataset_mixer][s_p : s_p_val_test]),
+            'labels': np.asarray(
+                [_['class'] for _ in dataset_mixer][s_p : s_p_val_test])}
+        self.test = {
+            'data': np.asarray(
+                [_['image'] for _ in dataset_mixer][s_p_val_test : ]),
+            'labels': np.asarray(
+                [_['class'] for _ in dataset_mixer][s_p_val_test : ])}
         # Convert to one hot labels
         tmp_labels = self.train['labels']
         self.train['labels'] = np.zeros((len(tmp_labels), 2))
@@ -76,23 +85,33 @@ class LoadDataSet():
         self.val['labels'] = np.zeros((len(tmp_labels), 2))
         self.val['labels'][range(len(tmp_labels)), tmp_labels] = 1
 
+        tmp_labels = self.test['labels']
+        self.test['labels'] = np.zeros((len(tmp_labels), 2))
+        self.test['labels'][range(len(tmp_labels)), tmp_labels] = 1
+
         # Reshape the images
         tmp = self.train['data'].shape
         self.train['data'].shape = (tmp[0], 1, tmp[1], tmp[2])
         tmp = self.val['data'].shape
         self.val['data'].shape = (tmp[0], 1, tmp[1], tmp[2])
+        tmp = self.test['data'].shape
+        self.test['data'].shape = (tmp[0], 1, tmp[1], tmp[2])
 
         # Normalise between -1 and 1
         # Get max value
         max_pixel = np.amax([np.amax(self.train['data']),
-                             np.amax(self.val['data'])])
+                             np.amax(self.val['data']),
+                             np.amax(self.test['data'])])
         print('Max pixel: ', max_pixel)
         self.train['data'] = self.train['data']/max_pixel*2 - 1
         self.val['data'] = self.val['data']/max_pixel*2 - 1
+        self.test['data'] = self.test['data']/max_pixel*2 - 1
         print('min self.train: ', np.amin(self.train['data']))
         print('max self.train: ', np.amax(self.train['data']))
         print('min self.val: ', np.amin(self.val['data']))
         print('max self.val: ', np.amax(self.val['data']))
+        print('min self.test: ', np.amin(self.test['data']))
+        print('max self.test: ', np.amax(self.test['data']))
 
 
     def show_images(self):
@@ -159,6 +178,17 @@ class LoadDataSet():
         out = torch.from_numpy(out).float()
         out = out.to(DEVICE)
         return out
+    def get_batch_test_all(self):
+        out = self.test['data']
+        out = torch.from_numpy(out).float()
+        out = out.to(DEVICE)
+        return out
+    def get_labels_test_all(self):
+        out = self.test['labels']
+        out = torch.from_numpy(out).float()
+        out = out.to(DEVICE)
+        return out
+
 
 
 
@@ -224,17 +254,19 @@ dtype = torch.float # not sure what this does
 #device = torch.device('cpu')
 # Globals:
 BATCH_SIZE = 25
-STEPS = 5000
+STEPS = 1500
 DEVICE = torch.device('cuda:2')
 STATS_STEPS = int(STEPS/100)
 if STATS_STEPS <= 1:
     STATS_STEPS = 5 # Every 5 steps get loss and accuracy stats
 
-dataset = LoadDataSet(0.9, BATCH_SIZE, DEVICE)
+dataset = LoadDataSet(0.8, BATCH_SIZE, DEVICE)
 print('dataset.train[data].shape: ', dataset.train['data'].shape)
 print('dataset.val[data].shape: ', dataset.val['data'].shape)
+print('dataset.test[data].shape: ', dataset.test['data'].shape)
 print('dataset.train[labels].shape: ', dataset.train['labels'].shape)
 print('dataset.val[labels].shape: ', dataset.val['labels'].shape)
+print('dataset.test[labels].shape: ', dataset.test['labels'].shape)
 net = Net(verbose=False)
 net = net.to(DEVICE) # Enable GPU
 input = torch.randn(1, 1, 210, 210, device = DEVICE)
@@ -255,17 +287,15 @@ criterion = nn.MSELoss()
 
 train_losses_step = []
 val_losses_step = []
-val_losses_smooth = []
 train_losses_smooth = []
+val_losses_smooth = []
+test_losses_smooth = []
 
 train_accuracies_step = []
 val_accuracies_step = []
-val_accuracies_smooth = []
-val_accuracies_simple = [] # DELETE LINE
-val_accuracies_new = [] # DELETE LINE
 train_accuracies_smooth = []
-train_accuracies_simple = [] # DELETE LINE
-delme_accuracy = []
+val_accuracies_smooth = []
+test_accuracies_smooth = []
 for i in range(STEPS):
     print('Step (', i, '/', STEPS, ')')
     if i % STATS_STEPS == 0:
@@ -281,7 +311,6 @@ for i in range(STEPS):
         train_accuracies_smooth.append(tmp_acc)
         train_losses_smooth.append(tmp_loss)
 
-
         # Calculate validation accuracy and loss based on all val images
         print('    VAL STATS...')
         tmp_acc, tmp_loss = (
@@ -293,6 +322,18 @@ for i in range(STEPS):
                 25))
         val_accuracies_smooth.append(tmp_acc)
         val_losses_smooth.append(tmp_loss)
+
+        # Calculate test accuracy and loss based on all val images
+        print('    TEST STATS...')
+        tmp_acc, tmp_loss = (
+            get_stats_epoch(
+                net,
+                criterion,
+                dataset.get_batch_test_all(),
+                dataset.get_labels_test_all(),
+                25))
+        test_accuracies_smooth.append(tmp_acc)
+        test_losses_smooth.append(tmp_loss)
 
     print('    OPTIMISE...')
     # Calculate loss and accuracy for single step
@@ -316,21 +357,6 @@ for i in range(STEPS):
     loss = criterion(output, labels_train)
     train_losses_step.append(loss.item())
 
-
-    # val accuracy redo
-    #output = net(batch_val)
-    #print('output.shape: {}'.format(output.shape))
-    #output = output.data.cpu().numpy() # (b, 2)
-    #output = output.round()
-    #print('output: {}'.format(output))
-    #print('labels: {}'.format(labels_val.cpu().numpy()))
-    #print('output == labels: {}'.format(output == labels_val.cpu().numpy()))
-    #print('output.shape: {}'.format(output.shape))
-    #accuracy = sum(output == labels_val.cpu().numpy())/len(output)
-    #accuracy = accuracy[0]
-    #delme_accuracy.append(accuracy)
-    #print('new accuracy: {}'.format(accuracy))
-
     # Calculate the validation accuracy and track
     output = net(batch_val)
     pred = np.zeros((len(labels_val), 2))
@@ -340,16 +366,10 @@ for i in range(STEPS):
     accuracy = accuracy[0]
     print('step val accuracy: {}'.format(accuracy))
 
-
-
     val_accuracies_step.append(accuracy)
     # Track losses
     loss_val = criterion(output, labels_val)
     val_losses_step.append(loss_val.item())
-
-    # Calculate and track validation accuracy
-
-
 
     # Compute gradients and optimise
     loss.backward()
@@ -362,12 +382,15 @@ plt.xlabel('Steps')
 plt.ylabel('Loss')
 #plt.plot(range(len(train_losses_step)), train_losses_step, label='train_step')
 #plt.plot(range(len(val_losses_step)), val_losses_step, label='val_step')
-plt.plot(range(0, len(val_losses_smooth)*STATS_STEPS, STATS_STEPS),
-         val_losses_smooth,
-         label='validation')
 plt.plot(range(0, len(train_losses_smooth)*STATS_STEPS, STATS_STEPS),
          train_losses_smooth,
-         label='train')
+         label='Train')
+plt.plot(range(0, len(val_losses_smooth)*STATS_STEPS, STATS_STEPS),
+         val_losses_smooth,
+         label='Validation')
+plt.plot(range(0, len(test_losses_smooth)*STATS_STEPS, STATS_STEPS),
+         test_losses_smooth,
+         label='Test')
 plt.grid(True)
 plt.legend()
 
@@ -376,12 +399,15 @@ plt.figure()
 plt.title('Accuracy')
 plt.xlabel('Steps')
 plt.ylabel('Accuracy')
-plt.plot(range(0, len(val_accuracies_smooth)*STATS_STEPS, STATS_STEPS),
-         val_accuracies_smooth,
-         label='validation')
 plt.plot(range(0, len(train_accuracies_smooth)*STATS_STEPS, STATS_STEPS),
          train_accuracies_smooth,
-         label='train')
+         label='Train')
+plt.plot(range(0, len(val_accuracies_smooth)*STATS_STEPS, STATS_STEPS),
+         val_accuracies_smooth,
+         label='Validation')
+plt.plot(range(0, len(test_accuracies_smooth)*STATS_STEPS, STATS_STEPS),
+         test_accuracies_smooth,
+         label='Test')
 #plt.plot(range(len(train_accuracies_step)), train_accuracies_step, label='train_step')
 #plt.plot(range(len(val_accuracies_step)), val_accuracies_step, label='val_step')
 plt.grid(True)
@@ -389,42 +415,38 @@ plt.legend()
 
 # ROC Training
 optimizer.zero_grad()
-fpr, tpr, auc= get_roc_curve(
+fpr_train, tpr_train, auc_train= get_roc_curve(
     net,
     dataset.get_batch_train_all(),
     dataset.get_labels_train_all(),
     optimizer,
     10)
-plt.figure()
-plt.title('ROC Curve - Training')
-plt.plot(fpr, tpr, label='Area = {:.2f}'.format(auc))
-plt.plot([0,1],[0,1], linestyle='dashed', color='k')
-plt.grid(True)
-plt.xlabel('False positive rate')
-plt.ylabel('True positive rate')
-plt.legend()
-print('fpr:\n', fpr)
-print('tpr:\n', tpr)
-
 # ROC Validation
 optimizer.zero_grad()
-fpr, tpr, auc= get_roc_curve(
+fpr_val, tpr_val, auc_val= get_roc_curve(
     net,
     dataset.get_batch_val_all(),
     dataset.get_labels_val_all(),
     optimizer,
     10)
+# ROC Test
+optimizer.zero_grad()
+fpr_test, tpr_test, auc_test= get_roc_curve(
+    net,
+    dataset.get_batch_test_all(),
+    dataset.get_labels_test_all(),
+    optimizer,
+    10)
 plt.figure()
-plt.title('ROC Curve - Validation')
-plt.plot(fpr, tpr, label='Area = {:.2f}'.format(auc))
+plt.title('ROC Curve - Training, Validation & Test')
+plt.plot(fpr_train, tpr_train, label='Train (area = {:.2f})'.format(auc_train))
+plt.plot(fpr_val, tpr_val, label='Validation (area = {:.2f})'.format(auc_val))
+plt.plot(fpr_test, tpr_test, label='Test - Area = {:.2f}'.format(auc_test))
 plt.plot([0,1],[0,1], linestyle='dashed', color='k')
 plt.grid(True)
 plt.xlabel('False positive rate')
 plt.ylabel('True positive rate')
 plt.legend()
-print('fpr:\n', fpr)
-print('tpr:\n', tpr)
-
 
 params = list(net.parameters())
 print(len(params))
